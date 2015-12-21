50f76b9d0862a2fbe2b6e23760b7b202ef3339d9
hanifsudira <sudirahanif@gmail.com>
2015-12-21 10:20:50 +0700

    ini progress gui server

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
-from Tkinter import Tk, Text, BOTH, W, N, E, S
-from ttk import Frame, Button, Label, Style
+from Tkinter import * #import Tk, Text, BOTH, W, N, E, S
+from ttk import * #Frame, Button, Label, Style
 import tkMessageBox
 
 class GUI(Frame):
@@ -10,6 +10,58 @@ class GUI(Frame):
 
 	def about(self):
 		tkMessageBox.showinfo("About", "Simple FTP Server\n\nGuruh Arya Sena\t5113100010\nBurhanudin Rasyid\t5113100046\nM. Ghulam Fajri\t5113100094\nHanif Sudira\t5113100184")
+
+	def ask_quit(self):
+		if tkMessageBox.askokcancel("Quit", "Anda Yakin Keluar?"):
+			self.quit()
+			
+	def add_user(self):
+
+		def function():
+			flag = False
+			username = e1.get()
+			password = e2.get()
+			inp = (username,password)
+			if inp:
+				with open('user.txt','rb') as f:
+					my_list = pickle.load(f)
+					print my_list
+				for x in range(len(my_list)):
+					if my_list[x][0] == inp[0]:
+						flag = True
+						break
+				if flag:
+					tkMessageBox.showerror("Error","User Telah Terdaftar")
+				else:
+					my_list.append(inp)
+					tkMessageBox.showinfo("Success","Berhasil Menambah User")
+					print my_list
+					flag = False
+
+
+		newwin = Tk()
+		newwin.geometry("300x100")
+		newwin.title("Tambah User")
+		newwin.resizable(width=False, height=False)
+		
+		Label(newwin, width=15, text="First Name").grid(row=0)
+		Label(newwin, width=15, text="Last Name").grid(row=1)
+
+		e1 = Entry(newwin)
+		e2 = Entry(newwin)
+
+		e1.grid(row=0, column=1)
+		e2.grid(row=1, column=1)
+
+		Button(newwin, text='Tambah', command=function).grid(row=3, column=1, sticky=W, pady=4)
+		Button(newwin, text='Batal', command=newwin.quit).grid(row=3, column=0, sticky=W, pady=4)
+		
+	def convert(self , tog=[0]):
+		tog[0] = not tog[0]
+		if tog[0]:
+			abtn.configure(text='Stop')
+		else:
+			self.configure(text='Start')
 
 	def initUI(self):
 		self.parent.title("Simple FTP Server")
@@ -27,19 +79,16 @@ class GUI(Frame):
 		area.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E+W+S+N)
 		area.insert('1.0',"wewewewew")
 
-		abtn = Button(self, text="Start")
+		abtn = Button(self, text="Start", command=self.convert)
 		abtn.grid(row=1, column=3)
 
-		cbtn = Button(self, text="Stop")
-		cbtn.grid(row=2, column=3, pady=4)
-
-		ubtn = Button(self, text="Add User")
+		ubtn = Button(self, text="Tambah User", command = self.add_user)
 		ubtn.grid(row=4 , column=3)
 		
 		hbtn = Button(self, text="About", command = self.about)
 		hbtn.grid(row=5, column=0, padx=5)
 
-		obtn = Button(self, text="OK")
+		obtn = Button(self, text="Quit", command = self.ask_quit)
 		obtn.grid(row=5, column=3)        
 
 if __name__ == '__main__':
