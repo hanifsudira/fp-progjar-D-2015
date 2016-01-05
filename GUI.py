01c14fd28f7d512c7eb78e54252def7dc5072609
guruharyasenna <guruharyasenna@gmail.com>
2016-01-05 07:46:27 +0700

    ui

--- GUI.py
+++ GUI.py
@@ -1,11 +1,309 @@
 from Tkinter import * #import Tk, Text, BOTH, W, N, E, S
 from ttk import * #Frame, Button, Label, Style
+from time import sleep
 import tkMessageBox
 import pickle
+import socket
+import threading as t
+import os
+import select
+
+#abtn = Button
+
+currentdirectory = os.path.abspath('.')
+allow_delete = True
+
+#def callback():
+#	 execfile("MultiClient-FTP.py")
+
+
+user = [('a','1')]
+
+ip 		= 'localhost'
+port 		= 12345
+max_client 	= 10
+
+
+class Server:
+	def __init__(self):
+		self.host = ip
+		self.port = port
+		self.backlog = 5
+		self.size = 1024
+		self.server = None
+		self.threads = []
+		#self.runrun=False
+
+	def open_socket(self):        
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.server.bind((self.host,self.port))
+		self.server.listen(max_client)
+		
+	def run(self, runrun=None):
+		self.open_socket()
+		input = [self.server, sys.stdin]
+		running = 1
+		while running:
+			inputready,outputready,exceptready = select.select(input,[],[])
+			for s in inputready:
+				if s == self.server:
+					# handle the server socket
+					c = Client(self.server.accept())
+					print c
+					c.start()
+					self.threads.append(c)
+				elif s == sys.stdin:
+					# handle standard input
+					junk = sys.stdin.readline()
+					running = 0
+		# close all threads
+		self.server.close()
+		for c in self.threads:
+			c.join()
+
+
+class Client(t.Thread):
+	def __init__(self,(connection,address)):
+		self.connection	= connection
+		self.address 	= address
+		t.Thread.__init__(self)
+		self.username 	= ''
+		self.rest=False
+		self.pasv_mode	= False
+		self.basedirectory = currentdirectory
+		self.currentdirectory = self.basedirectory
+		self.closeflag = True
+		self.total = ""
+
+	def run(self):
+		self.connection.send('220 Welcome!. ini adalah FTP sederhana kami :)\r\n')
+		# while True:
+		# 	command = self.connection.recv(1024)	
+		# 	if not command and self.closeflag :
+		# 		break
+		# 	else:
+		# 		print 'ini command : ', command.strip()
+		# 		try:
+		# 			function = getattr(self,command[:4].strip().upper())
+		# 			function(command)
+		# 		except Exception,e:
+		# 			print 'ERROR:',e
+		# 			self.connection.send('500 Syntax Error.\r\n')
+
+		running = 1
+		while running:
+			command = self.connection.recv(1024)
+			if command and self.closeflag:
+				print 'ini command : ',command.strip()
+				try:
+					function = getattr(self,command[:4].strip().upper())
+					function(command)
+				except Exception,e:
+					print 'ERROR:',e
+					self.connection.send('500 Syntax Error.\r\n')
+			else:
+				self.connection.close()
+				running = 0
+
+	def USER(self,command):
+		username = command.strip().split(' ')[1]
+		self.username = username
+		kirim = '331 Password required for '+username+'\r\n'
+		self.connection.send(kirim)
+
+	def PASS(self,command):
+		password = command.strip().split(' ')[1]
+		flag = False
+		for i in range(len(user)):
+			if self.username == user[i][0] and password == user[i][1]:
+				flag = True
+		if flag:
+			self.connection.send('230 Logged on\r\n')
+		else:
+			self.connection.send('530 Login or password incorrect!\r\n')
+			self.closeflag = False
+			#self.connection.close()
+
+	def QUIT(self,command):
+		self.connection.send('221 Goodbye.\r\n')
+		self.closeflag = False
+		#self.connection.close()
+
+	def MKD(self,command):
+		dirname=os.path.join(self.currentdirectory,command[4:-2])
+		os.mkdir(dirname)
+		self.connection.send('257 Directory created.\r\n')
+
+	def RMD(self,command):
+		dirname=os.path.join(self.currentdirectory,command[4:-2])
+		if allow_delete:
+			os.rmdir(dirname)
+			self.connection.send('250 Directory deleted.\r\n')
+		else:
+			self.connection.send('450 Not allowd.\r\n')
+
+	def DELE(self,command):
+		filename=os.path.join(self.currentdirectory,command[5:-2])
+		if allow_delete:
+			os.remove(filename)
+			self.connection.send('250 File delete.\r\n')
+		else:
+			self.connection.send('450 Not allowed.\r\n')
+
+	def AUTH(self,command):
+		self.connection.send('530 Please login with USER and PASS\r\n')
+
+	def SYST(self,command):
+		reply = "215 "+os.name+" "+platform.system()+" "+platform.system()+"\r\n"
+		self.connection.send(reply)
+
+	def TYPE(self,command):
+		self.mode = command[5]
+		self.connection.send('200 Binary mode.\r\n')
+
+	def REST(self,command):
+		self.pos=int(command[5:-2])
+		self.rest=True
+		self.connection.send('250 File position reseted.\r\n')
+
+	def HELP(self,command):
+		pesan = "214-The following commands are reconized\n USER PASS MKD RMD DELE SYST AUTH SYST TYPE REST\n HELP RETR STOR PWD CWD CDUP PASV RNFR RNTO LIST\n214 Help OK.\r\n"
+		self.connection.send(pesan)
+
+	def RETR(self,command):
+		filename=os.path.join(self.currentdirectory,command[5:-2])
+		print 'sedang mendownload:',filename
+		if self.mode=='I':
+			fileinput=open(filename,'rb')
+		else:
+			fileinput=open(filename,'r')
+		self.connection.send('150 Opening data connection.\r\n')
+		if self.rest:
+			fileinput.seek(self.pos)
+			self.rest=False
+		data= fileinput.read(1024)
+		self.start_datasock()
+		while data:
+			self.datasock.send(data)
+			data=fileinput.read(1024)
+		fileinput.close()
+		self.stop_datasock()
+		self.connection.send('226 Transfer complete.\r\n')
+
+	def STOR(self,command):
+		filename=os.path.join(self.currentdirectory,command[5:-2])
+		print 'sedang upload',filename
+		if self.mode=='I':
+			fileoutput=open(filename,'wb')
+		else:
+			fileoutput=open(filename,'w')
+		self.connection.send('150 Opening data connection.\r\n')
+		self.start_datasock()
+
+		while True:
+			data=self.datasock.recv(1024)
+			if not data: break
+			fileoutput.write(data)
+		fileoutput.close()
+		self.stop_datasock()
+		self.connection.send('226 Transfer complete.\r\n')
+
+
+	def PWD(self,command):
+		#relpath -> Return a relative filepath to path either from the current directory or from an optional start directory
+		printworkingdirectory = os.path.relpath(self.currentdirectory,self.basedirectory)
+		if printworkingdirectory == '.':
+			printworkingdirectory = '/'
+		else:
+			printworkingdirectory = '/' + printworkingdirectory
+		self.connection.send('257 \"%s\"\r\n' % printworkingdirectory)
+
+
+	def CWD(self,command):
+		changeworkingdirectory = command[4:-2]
+		if changeworkingdirectory =='/':
+			self.currentdirectory =self.basedirectory
+		elif changeworkingdirectory[0]=='/':
+			self.currentdirectory=os.path.join(self.basedirectory,changeworkingdirectory[1:])
+		else:
+			self.currentdirectory=os.path.join(self.currentdirectory,changeworkingdirectory)
+		self.connection.send('250 OK.\r\n')
+
+	def CDUP(self,command):
+		if not os.path.samefile(self.currentdirectory,self.basedirectory):
+			self.currentdirectory=os.path.abspath(os.path.join(self.currentdirectory,'..'))
+		self.connection.send('200 OK.\r\n')
+
+	def PASV(self,command):
+		self.pasv_mode 	= True
+		self.serversock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.serversock.bind((ip,0))
+		self.serversock.listen(1)
+		now_ip,now_port	= self.serversock.getsockname()
+		#print 'port for passive mode:',now_port
+		self.connection.send('227 Entering Passive Mode (%s,%u,%u).\r\n' %(','.join(now_ip.split('.')), now_port>>8&0xFF, now_port&0xFF))
+
+	def RNFR(self,command):
+		self.renamefilename=os.path.join(self.currentdirectory,command[5:-2])
+		self.connection.send('350 Ready.\r\n')
+
+	def RNTO(self,command):
+		filename = os.path.join(self.currentdirectory,command[5:-2])
+		os.rename(self.renamefilename,filename)
+		self.connection.send('250 File renamed.\r\n')
+
+	def start_datasock(self):
+		if self.pasv_mode:
+			self.datasock, addr = self.serversock.accept()
+			print 'connect:', addr
+		else:
+			self.datasock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			self.datasock.connect((self.dataAddr,self.dataPort))
+
+	def stop_datasock(self):
+		self.datasock.close()
+		if self.pasv_mode:
+			self.serversock.close()
+
+	def LIST(self,command):
+		self.total = ""
+		self.connection.send('150 Here comes the directory listing.\r\n')
+		print 'list:', self.currentdirectory
+		self.start_datasock()
+		for t in os.listdir(self.currentdirectory):
+			k=self.toListItem(os.path.join(self.currentdirectory,t))
+			self.total+=(k+'\r\n')
+		self.datasock.send(self.total)
+		self.stop_datasock()
+		self.connection.send('226 Directory send OK.\r\n')
+
+	def toListItem(self,fn):
+		st=os.stat(fn)
+		fullmode='rwxrwxrwx'
+		mode=''
+		for i in range(9):
+			mode+=((st.st_mode>>(8-i))&1) and fullmode[i] or '-'
+		d=(os.path.isdir(fn)) and 'd' or '-'
+		ftime=time.strftime(' %b %d %H:%M ', time.gmtime(st.st_mtime))
+		return d+mode+' 1 user group '+str(st.st_size)+ftime+os.path.basename(fn)
+
+
+class STDText(Text):
+   def __init__(self, parent):
+     Text.__init__(self, parent)
+
+   def write(self, stuff):
+     self.insert("end", stuff)
+     self.yview_pickplace("end")
+
+
 class GUI(Frame):
 	def __init__(self,parent):
 		Frame.__init__(self,parent)
+
 		self.parent = parent
+		self.runrun=False
 		self.initUI()
 
 	def about(self):
@@ -59,14 +357,9 @@ class GUI(Frame):
 		Button(newwin, text='Tambah', command=function).grid(row=3, column=1, sticky=W, pady=4)
 		Button(newwin, text='Batal', command=newwin.quit).grid(row=3, column=0, sticky=W, pady=4)
 		
-	def convert(self , tog=[0]):
-		tog[0] = not tog[0]
-		if tog[0]:
-			abtn.configure(text='Stop')
-		else:
-			self.configure(text='Start')
-
+	
 	def initUI(self):
+		#self.runrun=False
 		self.parent.title("Simple FTP Server")
 		self.pack(fill=BOTH, expand=True)
 
@@ -77,13 +370,16 @@ class GUI(Frame):
 		
 		lbl = Label(self, text="Command Line")
 		lbl.grid(sticky=W, pady=4, padx=5)
-		
-		area = Text(self)
+		area = STDText(self)
 		area.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E+W+S+N)
-		area.insert('1.0',"wewewewew")
+		#area.insert('1.0',"wewewewew")
+		sys.stdout=area		
 
-		abtn = Button(self, text="Start", command=self.convert)
-		abtn.grid(row=1, column=3)
+		self.abtn = Button(self, text="Start", command=lambda:self.execute(True))
+		self.abtn.grid(row=1, column=3)
+
+		self.bbtn = Button(self, text="Stop", command=lambda:self.execute(False))
+		self.bbtn.grid(row=2, column=3)		
 
 		ubtn = Button(self, text="Tambah User", command = self.add_user)
 		ubtn.grid(row=4 , column=3)
@@ -92,9 +388,30 @@ class GUI(Frame):
 		hbtn.grid(row=5, column=0, padx=5)
 
 		obtn = Button(self, text="Quit", command = self.ask_quit)
-		obtn.grid(row=5, column=3)        
+		obtn.grid(row=5, column=3) 
+       
+	def execute(self, runrun=None):
+ 	 if runrun is not None:
+	  self.runrun = runrun
+	 if self.runrun:
+	  FTP = Server()
+	  print 'connect on ', ip,':',port
+	  FTP.run()
+	  self.parent.after(5000, self.execute)	
+	  #raw_input('Enter untuk mengakhiri...\n')
+	
+	#def convert(self , tog=[0]):
+	#	tog[0] = not tog[0]
+	#	if tog[0]:
+	#		self.abtn.config(text='Stop')
+	#	else:
+	#		self.abtn.config(text='Start', command=main)
 
-if __name__ == '__main__':
+	
+	
+
+
+if __name__ == '__main__':	
 	root = Tk()
 	root.geometry("640x480")
 	root.resizable(width=False, height=False)
--- client.py
+++ client.py
@@ -1,6 +1,202 @@
 from Tkinter import Tk, Text, TOP, BOTH, X, N, Y, LEFT, RIGHT, RAISED, TOP, W, BOTTOM,NW,NE,E,SW
 from ttk import Frame, Label, Entry, Button, Style
+import os
+import socket 
 
+
+currentdirectory = os.path.abspath('.')
+
+def callback():
+		execfile("FTP-CLient.py")
+
+class Client:
+	def __init__(self,(ip,port)):
+		self.serverip 		= ip
+		self.port 		= port
+		self.server 		= None
+		self.data_sock		= None
+		self.recvdata		= ""
+		self.currentdirectory	= currentdirectory
+		self.isi		= ""
+	
+	def opensocket(self):
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server.connect((self.serverip,self.port))
+		welcome_msg = self.server.recv(1024)
+		print welcome_msg.strip()
+	
+	def run(self):
+		self.opensocket()
+		while True:
+			command = raw_input('')
+			if not command:
+				break
+			else:
+				print 'ini command' , command
+				try:
+					function = getattr(self,command[:4].strip().upper())
+					function(command)
+				except Exception,e:
+					print 'ERROR:',e
+
+	def USER(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def PASS(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def HELP(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def QUIT(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def SYST(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def TYPESTOR(self):
+		com="TYPE I\r\n"
+		self.server.send(com)
+		msg = self.server.recv(1024)
+		print msg.strip()	
+
+	def TYPERETR(self):
+		com="TYPE A\r\n"
+		self.server.send(com)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+
+	def PWD(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def CWD(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def CDUP(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def MKD(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def RNTO(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def RNFR(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def RMD(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def DELE(self,command):
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def PASV(self):
+		command="PASV\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+		data = msg.strip() 
+		if "Entering Passive Mode" in msg:
+			tp = data.split('(')[1].split(')')[0].split(',')
+			port = int(tp[4])*256+int(tp[5])
+		return port 
+
+	def LIST(self,command):
+		port=self.PASV()
+		self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.data_sock.connect(('localhost',port))
+		self.server.send(command)
+		self.recvdata=self.data_sock.recv(1024)
+		print self.recvdata.strip()
+		msg = self.server.recv(1024)
+		print msg.strip()
+		msg = self.server.recv(1024)
+		print msg.strip()
+	
+	def STOR(self,command):
+		filename=os.path.join(self.currentdirectory,command[5:])
+		self.TYPESTOR()
+		port=self.PASV()
+		self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.data_sock.connect(('localhost',port))
+
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+		
+		f = open(filename,'rb')
+		l = f.read()
+		f.close()
+		self.data_sock.sendall(l)
+		self.data_sock.close()
+		
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+	def RETR(self,command):
+		filename=os.path.join(self.currentdirectory,command[5:])
+		self.TYPERETR()
+		port=self.PASV()
+		self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.data_sock.connect(('localhost',port))
+		#self.data_sock.recv(1024)
+		#self.data_sock.recv(1024)
+		command+="\r\n"
+		self.server.send(command)
+		msg = self.server.recv(1024)
+		print msg.strip()
+
+		with open (filename, 'wb') as f:
+			self.isi = self.data_sock.recv(4096)
+			while (self.isi):
+				if not self.isi: break
+				else:
+					f.write(self.isi)
+					self.isi = self.data_sock.recv(4096)
+
+		msg = self.server.recv(1024)
+		print msg.strip()
 
 class Example(Frame):
   
@@ -9,50 +205,31 @@ class Example(Frame):
 		 
 		self.parent = parent
 		self.initUI()
-
 		
 	def initUI(self):
 		
 		self.parent.title("Simple FTP-Client")
 		self.pack(fill=BOTH, expand=True)
 	
-		# self.parent.title("Buttons")
-		
-		f3 = Frame(self)
-		f3.pack(expand=True)
-		lb=Label(f3, text="Password:").grid(row=0, column=0)
-		e3 = Entry(f3)
-		e3.grid(row=0, column=1, padx=5)
-		
-		# f4 = Frame(self)
-		# f4.pack(expand=True)
-		# lb4=Label(f4, text="Port:").grid(row=0, column=2)
-		# e4 = Entry(f4)
-		# e4.grid(row=0, column=3, padx=5)
-		# self.style = Style()
-		# self.style.theme_use("default")
-		# self.pack(fill=BOTH, expand=True)
-		# frame = Frame(self, relief=RAISED, borderwidth=1)
-		# frame.pack(side=BOTTOM,fill=BOTH, expand=True)
-		
-		
-		
-			  
-		
-		
-		# frame2 = Frame(self)
-		# frame2.pack(anchor=W,side=LEFT)
-		# lbl2 = Label(frame2, text="Command Line", width=6)
-		# lbl2.pack(fill=X,anchor=W,expand=True,padx=5, pady=5)        
-		# txt2 = Text(frame2)
-		# txt2.pack(fill=X,side=BOTTOM,pady=5, padx=5)           
+		# self.parent.title("Buttons") 
+				
+		#frame2 = Frame(self)
+		#frame2.pack(anchor=W,side=LEFT)
+		#lbl2 = Label(frame2, text="Command Line", width=6)
+		#lbl2.pack(fill=X,anchor=W,expand=True,padx=5, pady=5)        
+		#txt2 = Text(frame2)
+		#txt2.pack(fill=X,side=BOTTOM,pady=5, padx=5)           
 
-		frame3 = Frame(self)
-		frame3.pack(side=BOTTOM,anchor=SW)
+		frame3 = Frame(self, width=400, height=500)
+		frame3.pack(side=BOTTOM,anchor=NW)
 		lbl3 = Label(frame3, text="Command Line", width=6)
-		lbl3.pack(fill=BOTH,anchor=SW,expand=True,padx=5, pady=5)        
+		lbl3.pack(fill=X,anchor=NE,expand=True,padx=5, pady=5)        
 		txt = Text(frame3)
-		txt.pack(fill=BOTH,pady=5, padx=5)           
+		txt.pack(fill=BOTH,pady=5, padx=5)
+	        wid = frame3.winfo_id()
+		os.system('xterm -into %d -geometry 400x400 -sb &' % wid)
+		
+		     
 		
 		f1 = Frame(self)
 		f1.pack(fill=X,side=LEFT, anchor=NW, padx=5, pady=10)
@@ -77,30 +254,17 @@ class Example(Frame):
 		lb4=Label(f4, text="Port:").grid(row=0, column=2)
 		e4 = Entry(f4)
 		e4.grid(row=0, column=3, padx=5)
-		
-		connectButton = Button(self, text="Connect")
+
+		connectButton = Button(self, text="Connect", command=callback)
 		connectButton.pack(expand=True,fill=X,side=LEFT, anchor=NW, padx=10, pady=10)
-			
-		# frame1 = Frame(self)
-		# frame1.pack(fill=X)
-		# lbl1 = Label(frame1, text="Title", width=6)
-		# lbl1.pack(side=LEFT, padx=5, pady=5)           
-		# entry1 = Entry(frame1)
-		# entry1.pack(fill=Y, padx=0, expand=True)
 		
-		# frame2 = Frame(self)
-		# frame2.pack(fill=X)
-		# lbl2 = Label(frame2, text="Author", width=6)
-		# lbl2.pack(side=LEFT, padx=5, pady=5)        
-		# entry2 = Entry(frame2)
-		# entry2.pack(fill=X, padx=5, expand=True)
-		
+#def runnewclient(self, event)
+ #teks = open('FTP-CLient.py', 'r').read()
 		
 
 def main():
-  
 	root = Tk()
-	root.geometry("850x500")
+	root.geometry("1300x720")
 	app = Example(root)
 	root.mainloop()  
 	root.resizable(width=False, height=False)
